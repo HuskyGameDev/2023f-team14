48ef93eb1ec981a6bf56895623b7ff9ad4783057
Konr-B <114245091+Konr-B@users.noreply.github.com>
2023-09-27 22:22:14 -0400

    Fundamental Client & Server/Host Connectivity
    Clients can connect to a host and both (or more) players are able to move around. The position/state of clients and host is recognized by everyone connected to said host.

---
+++ Assets/PlayerNetwork.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class PlayerNetwork : NetworkBehaviour
+{
+    Vector3 moveDir = new Vector3(0,0,0);
+    // Update is called once per frame
+    private void Update()
+    {
+        if(!IsOwner){
+            return;
+        } 
+        if(Input.GetKeyDown(KeyCode.W)){
+            moveDir.z = +1f;
+        }else if(Input.GetKeyUp(KeyCode.W)){
+            moveDir.z = 0;
+        }
+        if (Input.GetKey(KeyCode.S)){
+            moveDir.z = -1f;
+        }else if(Input.GetKeyUp(KeyCode.S)){
+            moveDir.z = 0;
+        }
+        if (Input.GetKey(KeyCode.A)){
+            moveDir.x = +1f;
+        }else if(Input.GetKeyUp(KeyCode.A)){
+            moveDir.x = 0;
+        }
+        if (Input.GetKey(KeyCode.D)){
+            moveDir.x = -1f;
+        }else if(Input.GetKeyUp(KeyCode.D)){
+            moveDir.x = 0;
+        }
+
+        float moveSpeed = 3f;
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
+    }    
+}
